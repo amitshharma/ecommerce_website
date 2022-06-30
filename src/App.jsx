6c1d7b9ff8e877424a00b4caed140db3524560ca
Amit Sharma <99009356+amitshharma@users.noreply.github.com>
2022-07-01 02:37:48 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,26 @@ import ProductList from "./pages/ProductList";
 import Register from "./pages/Register";
 import Login from "./pages/Login";
 import Cart from "./pages/Cart";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
 
 const App = () => {
-  return <Cart/>;
+  return (
+  <Router>
+    <Routes>
+        <Route exact path='/' element={<Home />} />
+        <Route path='/product' element={<ProductList />} />
+        <Route path='/cart' element={<Cart />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/product' element={<Product />} />
+        <Route path='/register' element={<Register />} />
+        <Route path='/home' element={<Home />} />
+    </Routes>
+  </Router>
+  );
 };
 
 export default App;
